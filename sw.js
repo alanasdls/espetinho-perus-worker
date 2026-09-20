4e59964b0447f176aa991e1754a6a18b9b7f0540
alanasdls <alanasdls@gmail.com>
2026-09-20 16:15:42 -0300

    chore: refresh cache for V122 benefits

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // V105-BANNERS-HARDWIRED
-const CACHE = 'espetinho-perus-v121-benefits-reference';
+const CACHE = 'espetinho-perus-v122-benefits-reference';
 const PRECACHE = [
-  './', './index.html', './pedido.html','./cliente.html','./cliente.css?v=20260815-v95-fidelidade-resgate','./cliente.js?v=20260920-v119-register-direct', './pedido.css?v=20260724-v2', './pedido.js?v=20260724-v3', './admin.html', './admin.css?v=20260727-v79-admin-studio', './admin.js?v=20260727-v79-admin-studio', './admin-catalog.json?v=20260727-v79', './styles-v105.css?v=20260920-v121-benefits-reference', './app-misticpay-cpf-v102.js?v=20260920-v110-assets', './pagamento-sucesso.html', './pagamento-pendente.html', './pagamento-falhou.html',
+  './', './index.html', './pedido.html','./cliente.html','./cliente.css?v=20260815-v95-fidelidade-resgate','./cliente.js?v=20260920-v119-register-direct', './pedido.css?v=20260724-v2', './pedido.js?v=20260724-v3', './admin.html', './admin.css?v=20260727-v79-admin-studio', './admin.js?v=20260727-v79-admin-studio', './admin-catalog.json?v=20260727-v79', './styles-v105.css?v=20260920-v122-benefits-reference', './app-misticpay-cpf-v102.js?v=20260920-v110-assets', './pagamento-sucesso.html', './pagamento-pendente.html', './pagamento-falhou.html',
   './manifest.webmanifest', './manifest-admin.webmanifest', './banner-v105-01-10-desconto.jpg', './banner-v105-02-karaoke-double.jpg', './banner-v105-03-instagram-musica.jpg', './alerta-pedido.wav?v=20260724-v4', './icon-192.png', './icon-512.png', './apple-touch-icon.png',
   './assets/products/azeitona-20260723.webp',
   './assets/products/batata-com-calabresa-ou-frango-600g-20260723.webp',
